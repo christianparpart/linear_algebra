4064b23034528d9fc55e7663984bf3e11c19ae36
Christian Parpart <christian@parpart.family>
2020-02-15 02:17:01 +0100

    tests: back to constexpr

--- test/mul.cpp
+++ test/mul.cpp
@@ -20,46 +20,46 @@
 
 TEST_CASE("multiplication: vector * vector")
 {
-    auto static const v1 = ivec<3>{5, 2, 3};
-    auto static const v2 = ivec<3>{1, 2, 5};
-    auto static const v3 = v1 * v2;
-    auto static const expected = 24;
+    auto static constexpr v1 = ivec<3>{5, 2, 3};
+    auto static constexpr v2 = ivec<3>{1, 2, 5};
+    auto static constexpr v3 = v1 * v2;
+    auto static constexpr expected = 24;
     REQUIRE(v3 == expected);
 }
 
 TEST_CASE("multiplication: scalar * vector")
 {
-    auto static const v1 = ivec<3>{5, 2, 3};
-    auto static const s1 = 4;
-    auto static const v2 = s1 * v1;
-    auto static const expected = ivec<3>{20, 8, 12};
+    auto static constexpr v1 = ivec<3>{5, 2, 3};
+    auto static constexpr s1 = 4;
+    auto static constexpr v2 = s1 * v1;
+    auto static constexpr expected = ivec<3>{20, 8, 12};
     REQUIRE(v2 == expected);
 }
 
 TEST_CASE("multiplication: vector * scalar")
 {
-    auto static const v1 = ivec<3>{5, 2, 3};
-    auto static const s1 = 4;
-    auto static const v2 = v1 * s1;
-    auto static const expected = ivec<3>{20, 8, 12};
+    auto static constexpr v1 = ivec<3>{5, 2, 3};
+    auto static constexpr s1 = 4;
+    auto static constexpr v2 = v1 * s1;
+    auto static constexpr expected = ivec<3>{20, 8, 12};
     REQUIRE(v2 == expected);
 }
 
 TEST_CASE("multiplication: scalar * matrix")
 {
-    auto static const m1 = imat<2, 3>{1, 2, 3, 4, 5, 6};
-    auto static const s1 = 2;
-    auto static m2 = s1 * m1;
-    auto static expected = imat<2, 3>{2, 4, 6, 8, 10, 12};
+    auto static constexpr m1 = imat<2, 3>{1, 2, 3, 4, 5, 6};
+    auto static constexpr s1 = 2;
+    auto static constexpr m2 = s1 * m1;
+    auto static constexpr expected = imat<2, 3>{2, 4, 6, 8, 10, 12};
     REQUIRE(m2 == expected);
 }
 
 TEST_CASE("multiplication: matrix * scalar")
 {
-    auto static const m1 = imat<2, 3>{1, 2, 3, 4, 5, 6};
-    auto static const s1 = 2;
-    auto static m2 = m1 * s1;
-    auto static expected = imat<2, 3>{2, 4, 6, 8, 10, 12};
+    auto static constexpr m1 = imat<2, 3>{1, 2, 3, 4, 5, 6};
+    auto static constexpr s1 = 2;
+    auto static constexpr m2 = m1 * s1;
+    auto static constexpr expected = imat<2, 3>{2, 4, 6, 8, 10, 12};
     REQUIRE(m2 == expected);
 }
 
--- test/sub.cpp
+++ test/sub.cpp
@@ -20,18 +20,18 @@
 
 TEST_CASE("subtraction: vector - vector")
 {
-    auto static const v1 = ivec<3>{5, 2, 1};
-    auto static const v2 = ivec<3>{1, 2, 5};
-    auto static const v3 = v1 - v2;
-    auto static const expected = ivec<3>{4, 0, -4};
+    auto static constexpr v1 = ivec<3>{5, 2, 1};
+    auto static constexpr v2 = ivec<3>{1, 2, 5};
+    auto static constexpr v3 = v1 - v2;
+    auto static constexpr expected = ivec<3>{4, 0, -4};
     REQUIRE(v3 == expected);
 }
 
 TEST_CASE("subtraction: matrix - matrix")
 {
-    auto static const m1 = imat<2, 2>{5, 2, 1, 0};
-    auto static const m2 = imat<2, 2>{0, 2, 4, 8};
-    auto static const m3 = m1 - m2;
-    auto static const expected = imat<2, 2>{5, 0, -3, -8};
+    auto static constexpr m1 = imat<2, 2>{5, 2, 1, 0};
+    auto static constexpr m2 = imat<2, 2>{0, 2, 4, 8};
+    auto static constexpr m3 = m1 - m2;
+    auto static constexpr expected = imat<2, 2>{5, 0, -3, -8};
     REQUIRE(m3 == expected);
 }
